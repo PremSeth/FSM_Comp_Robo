c5e587bfb7705f8ff8c8ad4c1d8fc26746eabc5e
PremSeth <psethumadhavan@olin.edu>
2026-09-17 08:55:59 -0400

    FSM

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'in_class_day05'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='prem',
+    maintainer_email='psethumadhavan@olin.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'wall_approach = in_class_day05.wall_approach:main'
+        ],
+    },
+)
